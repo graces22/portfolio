cb1787877380544e3d1bdb47078a06821a9c4369
graces22 <gracesong1106@hotmail.com>
2022-08-09 19:25:18 -0400

    testing6

--- Components/Projectcards.js
+++ Components/Projectcards.js
@@ -11,26 +11,22 @@ function Projectcards() {
       <Element id="Projects" name="Projects">
            <div className="flex-col justify-start text-center mx-5 md:mx-6 lg:mx-8 py-44 space-y-40">
 
-           <div className="flex-col md:flex md:flex-row justify-start">
-                 <div className="w-full md:w-1/2">
+           <div className="flex-col md:flex md:flex-row justify-between md:space-x-7">
+                 <div className="w-full md:w-1/2 md:flex-1">
                   <a href="/tapqr"><Image src={Tqrherocard} alt="hero" layout="responsive" objectFit="cover" className="p-2 cursor-pointer"/></a>
                  </div>
 
-                 <div className="flex flex-col w-full md:w-1/2 md:ml-6 lg:ml-8 justify-between"> 
+                 <div className="md:flex flex-col w-full md:w-1/2 md:ml-6 lg:ml-8 justify-between"> 
                    
-                   <div className="mt-5 md:mt-0">
-                      <a href="/tapqr"><p className="font-normal text-left text-4xl lg:text-5xl cursor-pointer">TapQR</p></a>
-                    <div className="py-5">
+                   <div className="mt-5 md:mt-0 space-y-4">
+                      <a href="/tapqr" className="font-normal flex text-left text-4xl lg:text-5xl cursor-pointer">TapQR</a>
                       <div className="w-full border-t border-black border-dotted"></div>
-                    </div>
                     <p className="flex mt-0 text-left text-xl lg:text-2xl break-normal text-zinc-400">Designing a seamless experience to leverage QR codes—built on the Solana blockchain.</p>
                    </div>
 
-                  <div className="mt-14 md:mt-0">
+                  <div className="mt-14 md:mt-0 space-y-4">
                    <p className="font-['Maison_Neue'] text-xs md:text-sm text-left">DESKTOP WEB · MOBILE APP · UI/UX CASE STUDY</p>
-                     <div className="pt-4">
                       <div className="w-full border-t border-black border-dotted"></div>
-                     </div>
                   </div>
 
                 </div>
