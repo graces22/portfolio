074dcc1e0c6ed4ca73be9bbb1483735a99b468d8
graces22 <gracesong1106@hotmail.com>
2022-08-22 19:23:28 -0400

    initial commit v1.8

--- Components/Intro.js
+++ Components/Intro.js
@@ -7,19 +7,19 @@ function Intro() {
       <div className="flex flex-col justify-between text-center mx-0 md:mx-2 lg:mx-3 pt-28">
          <div className="md:ml-3 mx-5 mt-0 overflow-hidden h-[5rem] md:h-[6.5rem] lg:h-[8.5rem]">
             <ul className="animate-scrollUp space-y-6 pb-10">
-             <li className="flex font-normal text-7xl md:text-8xl lg:text-9xl text-left">
+             <li className="flex font-normal text-[64px] md:text-8xl lg:text-9xl text-left">
                 Passionate.
              </li>
-             <li className="flex font-normal text-7xl md:text-8xl lg:text-9xl text-left">
+             <li className="flex font-normal text-[64px] md:text-8xl lg:text-9xl text-left">
                 Inquisitive.
              </li>
-             <li className="flex font-normal text-7xl md:text-8xl lg:text-9xl text-left">
+             <li className="flex font-normal text-[64px] md:text-8xl lg:text-9xl text-left">
                 Resilient.
              </li>
-             <li className="flex font-normal text-7xl md:text-8xl lg:text-9xl text-left">
+             <li className="flex font-normal text-[64px] md:text-8xl lg:text-9xl text-left">
                 Thoughtful.
              </li>
-             <li className="flex font-normal text-7xl md:text-8xl lg:text-9xl text-left">
+             <li className="flex font-normal text-[64px] md:text-8xl lg:text-9xl text-left">
                 Collaborative.
              </li>
              
